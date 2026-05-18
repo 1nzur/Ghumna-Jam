c9707635377e6d69b176f414221feda079a5ad60
Samul Joshi <notsamulxd@gmail.com>
2026-05-18 23:06:09 +0545

    Updated run.py and added new app modules

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
-from app import create_app
-app = create_app()
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5055)
