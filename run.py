41421af38c4c88556e2e97d85202054535f5e57a
Samul Joshi <notsamulxd@gmail.com>
2026-05-20 21:43:35 +0545

    made signup page

--- run.py
+++ run.py
@@ -14,5 +14,10 @@ def home():
 def login():
     return render_template("login.html")
 
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
 if __name__ == "__main__":
     app.run(debug=True, port=5055)
