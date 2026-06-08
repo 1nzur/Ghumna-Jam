e6d15dc44bee3e2012371c885f85141243fc77f7
Samul Joshi <notsamulxd@gmail.com>
2026-06-08 10:25:41 +0545

    added offline gps tracking

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -48,6 +48,9 @@ class AuthRoutes:
         self.bp.route("/destination/<int:dest_id>")(
             self.controller.destination_detail
         )
+        self.bp.route("/compare")(
+            self.controller.compare_treks
+        )
 
         self.bp.route("/book/<int:dest_id>", methods=["POST"])(
             self.controller.book_trip
@@ -59,6 +62,10 @@ class AuthRoutes:
 
         self.bp.route("/profile", methods=["GET", "POST"])(
             self.controller.edit_profile
+        )
+
+        self.bp.route("/tracking")(
+            self.controller.tracking
         )
 
         return self.bp
