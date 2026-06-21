f0d6a45d7d4b013458904aa60bdb7419440bd1c2
Biggyan kharel <kharelbiggyan@gmail.com>
2026-06-21 10:12:33 +0545

    Fix forgot password - implement real email sending with Gmail SMTP

--- config.py
+++ config.py
@@ -3,3 +3,8 @@ MYSQL_HOST = "localhost"
 MYSQL_USER = "root"
 MYSQL_PASSWORD = "Arlonoobno1"
 MYSQL_DATABASE = "ghumna_jam"
+
+# Gmail SMTP for password reset emails
+# Generate an App Password at: https://myaccount.google.com/apppasswords
+MAIL_USERNAME = "kharelbiggyan@gmail.com"
+MAIL_PASSWORD = "lsizbcwibpjfvhlr"
